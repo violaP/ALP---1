9dce6ba710717aa35fea4e56b0edd3e65ebdbe63
Jonni <j.zint@fu-berlin.de>
2011-10-29 17:40:25 +0200

    added Aufgabe 2

--- 2/u2.hs
+++ 2/u2.hs
@@ -8,3 +8,10 @@ clockdiff (h1, m1) (h2, m2)	| h1*60+m1 > h2*60+m2	= maketime (-((h1*60+m1)-(h2*6
 maketime :: Int -> (Int, Int)
 maketime mins | mins >= 0 = ((div mins 60), (mod mins 60))
               | otherwise = (-(div (-mins) 60), -(mod (-mins) 60))
+
+{- Aufgabe 2 -}
+
+timeformat :: (Int, Int) -> String
+timeformat (h, m) | h > 23 || m > 59 || m < 0 = "ungueltig"
+		  | h >= 13		 = show (h-12) ++ ":" ++ show m ++ " p.m."
+		  | otherwise 		 = show h ++ ":" ++ show m ++ " a.m."
