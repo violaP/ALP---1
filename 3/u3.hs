466b48fc15b5614439f11caaba03bb0b404080ee
Jonni <j.zint@fu-berlin.de>
2011-11-08 12:05:46 +0100

    sync

--- 3/u3.hs
+++ 3/u3.hs
@@ -8,3 +8,10 @@ wsmthanav xs = smthanav xs (length xs) 0
 smthanav :: [Integer] -> Int -> Int  -> Int
 smthanav xs 0 r = r
 smthanav xs i r = smthanav xs (i-1) (if (fromInteger(xs!!(i-1))) < ((fromInteger (sum xs)) /  (fromIntegral (length xs))) then (r+1) else r)
+
+{- Aufgabe 14 -}
+
+type Artikel = ([Char] s, Eq s)=> [(s, Float)]
+
+preis :: Artikel -> Artikel -> (Float,[String])	
+preis p e = (1, [(filter (\x -> fst (elem p) == fst(elem x))) e])
